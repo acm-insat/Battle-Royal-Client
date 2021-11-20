6415cd9c15ab866e44e527e1e274fe99ff0e332c
Nie-Mand <chill.out.x19@gmail.com>
2021-11-21 00:21:10 +0100

    too bad for real time

--- src/config/ApolloClient.ts
+++ src/config/ApolloClient.ts
@@ -1,6 +1,5 @@
 import { split, ApolloClient, createHttpLink, InMemoryCache, concat } from '@apollo/client'
 import { getMainDefinition } from '@apollo/client/utilities'
-import { WebSocketLink } from '@apollo/client/link/ws';
 import { setContext } from '@apollo/client/link/context'
 import { getHeader } from '../shared/helpers'
 
@@ -14,31 +13,8 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-
-const wsLink = new WebSocketLink({
-  uri: `${process.env.SNOWPACK_PUBLIC_SOCKET_URL}/`,
-  options: {
-    // reconnect: true,
-    connectionParams: {
-      authToken: `Bearer ${localStorage.getItem('token')}`,
-    },
-  }
-})
-
-const connectionLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  authLink.concat(httpLink)
-)
-
 const client = new ApolloClient({
-  link: connectionLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     addTypename: false,
   }),
